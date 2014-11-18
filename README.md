f45a3f69927ca1dcb04f6e70338c3af01a9245da
Denis Stepulenok <super.denis@gmail.com>
2014-11-19 02:36:59 +0300

    Кнопки 0-9

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Загрузка SceneBuilder
 http://www.oracle.com/technetwork/java/javase/downloads/javafxscenebuilder-info-2157684.html
+
+Д.з. калькулятор
--- src/calc/Controller.java
+++ src/calc/Controller.java
@@ -1,23 +1,24 @@
 package calc;
 
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class Controller {
 
-    public Button calcButton;
     public TextField aField;
-    public TextField bField;
     public TextField resultField;
     public VBox mainPanel;
 
-    public void calcSum(ActionEvent actionEvent) {
-        System.out.println("Controller.calcSum");
-        double a = Double.parseDouble(aField.getText());
-        double b = Double.parseDouble(bField.getText());
-        double sum = a + b;
-        resultField.setText(Double.toString(sum));
-    }
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    public Button button0;
+
 }
