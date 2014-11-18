9ea9db0cd47410a73369c15f09447747c61d1676
Denis Stepulenok <super.denis@gmail.com>
2014-11-18 22:27:56 +0300

    Суммирование работает

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    public Button calcButton;
+    public TextField aField;
+    public TextField bField;
+    public TextField resultField;
+
+    public void calcSum(ActionEvent actionEvent) {
+        System.out.println("Controller.calcSum");
+        double a = Double.parseDouble(aField.getText());
+        double b = Double.parseDouble(bField.getText());
+        double sum = a + b;
+        resultField.setText(Double.toString(sum));
+    }
 }
