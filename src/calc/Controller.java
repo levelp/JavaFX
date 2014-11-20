e3710080754a25ed8199bf1172d9ff7884e44f93
darya <darya.jukova@gmail.com>
2014-11-20 22:14:03 +0300

    Main functional

--- src/calc/Controller.java
+++ src/calc/Controller.java
@@ -8,7 +8,6 @@ import javafx.scene.layout.VBox;
 public class Controller {
 
     public TextField display;
-    public TextField resultField;
     public VBox mainPanel;
 
     public Button button1;
@@ -21,12 +20,73 @@ public class Controller {
     public Button button8;
     public Button button9;
     public Button button0;
+    public Button resultButton;
+    public Button sumButton;
+    public Button subButton;
+    public Button multButton;
+    public Button divButton;
+    public Button buttonClear;
+    public Button buttonPoint;
+
+    public double result1;
+    public double result2;
+    public String operation;
+    public String lastOperation;
+    boolean isFirstNumber = true;
+    boolean clearText = false;
+
 
     public void digit(ActionEvent actionEvent) {
         System.out.println("actionEvent = " + actionEvent.toString());
-        if (actionEvent.getSource() instanceof Button) {
-            Button button = (Button) actionEvent.getSource();
-            display.setText(display.getText() + button.getText());
+
+        Button button = (Button) actionEvent.getSource();
+        if (clearText) {
+            display.setText("");
+            clearText = false;
         }
+
+        display.setText(display.getText() + button.getText());
+
     }
+
+    public void clear(ActionEvent actionEvent) {
+        display.setText("");
+        isFirstNumber = true;
+    }
+
+    public void operation(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        result1 = Double.parseDouble(display.getText());
+        System.out.println("result is " + result1);
+        operation = button.getText();
+        if(isFirstNumber) {
+            result2 = result1;
+            lastOperation = operation;
+            isFirstNumber = false;
+        } else {
+            operate(result1);
+            lastOperation = operation;
+        }
+        clearText = true;
+
+    }
+
+    public void operate(double result1) {
+        if (lastOperation.equals("+")) {
+            result2 += result1;
+        } else if (lastOperation.equals("-")) {
+            result2 -=result1;
+        } else if (lastOperation.equals("*")) {
+            result2 *= result1;
+        } else if (lastOperation.equals("/")) {
+            result2 /=result1;
+        } else if (lastOperation.equals("=")) {
+            result2 = result1;
+        }
+        System.out.println("result2 is " + result2);
+        display.setText(Double.toString(result2));
+
+    }
+
+
 }
